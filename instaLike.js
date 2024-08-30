23be142047fc14081c542f1aa7a2c34f0c32bc57
PRNC01 <prnc.business23@gmail.com>
2024-08-30 22:22:07 +0000

    INSTA LIKE SCRIPT FILE

---
+++ instaLike.js
@@ -0,0 +1,6 @@
+const Con = document.getElementById('Img');
+const Like = document.querySelector('i');
+
+Con.addEventListener('click', () => {
+    Like.style.transform = 'translate(-50%, -50%) scale(1)'
+})
